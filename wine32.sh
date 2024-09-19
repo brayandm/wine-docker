ac813da8a19b35cfc61f6df9fe20b344b475003d
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-09-19 18:05:38 +0200

    refactor: Update Wine installation to include wine32 and pulseaudio packages

--- wine32.sh
+++ wine32.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 docker run \
-    --user $(id -u):$(id -g) \
+    --device /dev/snd \
+    -e PULSE_SERVER=unix:/run/user/1000/pulse/native \
+    -v /run/user/1000/pulse:/run/user/1000/pulse \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -e DISPLAY=$DISPLAY \
     -v /:/host wine32:latest bash \
-    -c "WINEPREFIX='/host$(pwd)/wine32files' wine '/host$(pwd)/$1'"
+    -c "useradd -u $(id -u) -g root -m $(whoami) && \
+        chown $(id -u):$(id -g) '/host' && \
+        chown -R $(id -u):$(id -g) /run/user/1000 && \
+        su $(whoami) -c \"WINEPREFIX='/host$(pwd)/wine32files' wine '/host$(pwd)/$1'\""
