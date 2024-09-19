c0e9443c5e8958491a1e8a09da58fd802bd440d5
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-09-19 16:16:36 +0200

    refactor: Update Wine scripts to use WINEPREFIX for better compatibility

--- wine64.sh
+++ wine64.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-docker run --user $(id -u):$(id -g) -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -v /:/host wine64:latest bash -c "WINEPREFIX='/host$(pwd)/wine64files' wine64 '/host$(pwd)/$1'"
+docker run \
+    --user $(id -u):$(id -g) \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -e DISPLAY=$DISPLAY \
+    -v /:/host wine64:latest bash \
+    -c "WINEPREFIX='/host$(pwd)/wine64files' wine64 '/host$(pwd)/$1'"
