d7f89ddbae738f0bbd0f3f99c655a5ba5c10b3f8
Brayan Durán Medina <brayanduranmedina@gmail.com>
2024-09-19 18:37:21 +0200

    refactor: Update Wine scripts to use WINEPREFIX for better compatibility

--- wine32.sh
+++ wine32.sh
@@ -2,12 +2,12 @@
 
 docker run \
     --device /dev/snd \
-    -e PULSE_SERVER=unix:/run/user/1000/pulse/native \
-    -v /run/user/1000/pulse:/run/user/1000/pulse \
+    -e PULSE_SERVER=unix:/run/user/$(id -u)/pulse/native \
+    -v /run/user/$(id -u)/pulse:/run/user/$(id -u)/pulse \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -e DISPLAY=$DISPLAY \
     -v /:/host wine32:latest bash \
     -c "useradd -u $(id -u) -g root -m $(whoami) && \
         chown $(id -u):$(id -g) '/host' && \
-        chown -R $(id -u):$(id -g) /run/user/1000 && \
+        chown -R $(id -u):$(id -g) /run/user/$(id -u) && \
         su $(whoami) -c \"WINEPREFIX='/host$(pwd)/wine32files' wine '/host$(pwd)/$1'\""
